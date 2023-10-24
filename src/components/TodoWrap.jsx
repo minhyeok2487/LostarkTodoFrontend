e52ac3f6ea6337820eff1c8136b5b5637b255b07
leehanbee35 <73865593+leehanbee35@users.noreply.github.com>
2023-10-25 00:51:49 +0900

    카던 팝업 수정 중

--- src/components/TodoWrap.jsx
+++ src/components/TodoWrap.jsx
@@ -340,20 +340,21 @@ export default function TodoWrap() {
 
     // 모달 열기 함수
     const openContentModal = (character, category) => {
-        setModalTitle("[" + character.characterName + "] " + category + " 평균 데이터");
+        setModalTitle("" + character.characterName + " " + category + " 평균 데이터");
         if (category === "카오스던전") {
             var modalContent = (
-                <div>
+                <div class="chaosVisual">
+                    <span class="tip">API 실시간 경매장 가격으로 평균 값을 가져옵니다.</span>
                     <ul>
-                        <p>이름 : {character.chaos.name}</p>
-                        ---거래 가능 재화---
-                        <li>파괴석 : {character.chaos.destructionStone}개</li>
-                        <li>수호석 : {character.chaos.guardianStone}개</li>
-                        <li>1레벨보석 : {character.chaos.jewelry}개</li>
-                        ---거래 불가 재화---
-                        <li>돌파석 : {character.chaos.leapStone}개</li>
-                        <li>실링 : {character.chaos.shilling}개</li>
-                        <li>파편 : {character.chaos.honorShard}개</li>
+                        <p>컨텐츠 : {character.chaos.name}</p>
+                        <strong>거래 가능 재화</strong>
+                        <li>파괴석 <em>{character.chaos.destructionStone}개</em></li>
+                        <li>수호석 <em>{character.chaos.guardianStone}개</em></li>
+                        <li>1레벨보석 <em>{character.chaos.jewelry}개</em></li>
+                        <strong>거래 불가 재화</strong>
+                        <li>돌파석 <em>{character.chaos.leapStone}개</em></li>
+                        <li>실링 <em>{character.chaos.shilling}개</em></li>
+                        <li>파편 <em>{character.chaos.honorShard}개</em></li>
                     </ul>
                 </div>
             );
